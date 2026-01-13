b60c5e1979d2ec1ceb68aa7ef82a84d680d713ee
Tejas Shah <tejasshah2k19@gmail.com>
2026-01-13 19:06:40 +0530

    command line , encapsulation

---
+++ CommandLineDemo.java
@@ -0,0 +1,14 @@
+public class CommandLineDemo {
+ 
+    public static void main(String[] args) {//0 
+
+        System.out.println(args[0]);//
+
+        System.out.println(args[1]);//
+
+        
+        System.out.println(args[0] + args[1]);//
+
+        //dir del rmdir mkdir add 10 20 
+    }
+}
