9cb133048f49f35c009eff693c5bec32b8cf37d6
Tejas Shah <tejasshah2k19@gmail.com>
2026-01-20 18:55:50 +0530

    ticket app : login

--- TicketBookingApp.java
+++ TicketBookingApp.java
@@ -1,8 +1,48 @@
 
+import java.util.Scanner;
+
+
 public class TicketBookingApp {
 
     public static void main(String rargs[]) {
-            Tickets tickets = new Tickets(); //constructor => 
+        Tickets tickets = new Tickets(); //constructor => 
+        Customer c = new Customer();
+
+        int choice;
+        Scanner scr = new Scanner(System.in); 
+
+        while (true) {
+            System.out.println("1 For Login\n2 For Exit\nEnter choice");
+            choice = scr.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter mobile num");
+                    String tmp = scr.next(); //mobile 
+                    
+                    if(c.mobileNum.equals("-1")){
+                        //new 
+                        c.mobileNum = tmp;                         
+                    }else{ 
+                        //existing
+                        if(c.mobileNum.equals(tmp)){
+                            //login success 
+                            //sub menu 
+                            
+                        }else{
+                            System.out.println("Invalid Credentials....");
+                        }
+                    }                         
+                    break;
+                case 2:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Input PTA");
+
+            }//switch login main menu 
+        }//while login main menu 
+
     }
 }
 
@@ -30,4 +70,16 @@ class Tickets {
 
 class Customer {
 
+    int totalGoldTicket;
+    int totalSilverTicket;
+    int totalDiamondTicket;
+
+    String mobileNum;//null 
+
+    Customer() {
+        totalDiamondTicket = 0;
+        totalGoldTicket = 0;
+        totalSilverTicket = 0;
+        mobileNum = "-1";
+    }
 }
